61e10c7a57d0e9bfcc12193b77e039d552406435
Nixson Pires <117226315+OliveiraNixson@users.noreply.github.com>
2025-08-06 11:27:22 -0300

    App integrado ao CoreML
    Utilizando o CoreMl para criar um app que possa fazer predições sobre o melhor horário de dormir baseado no numero do copos de cafe, quantidade de horas dormidas e hora para acordar.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by iredefbmac_27 on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
